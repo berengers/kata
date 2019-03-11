3cb76ee06389da7a982c12d4a2422509dc1a6b4b
Berenger <berenger.salmon@gmail.com>
2019-03-11 17:26:28 +0100

    feat: first proposition for anagrams kata

---
+++ anagrams/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace anagrams
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string wordToFind = "Atelier".ToLower();
+      string text = File.ReadAllText("./static/mission1.txt", Encoding.UTF8);
+      var arrayWords = text.Split("\n");
+      List<Array> matchWords = new List<Array>();
+
+      for (int i = 0; i < arrayWords.Length; i++)
+      {
+        for (int j = 0; j < i; j++)
+        {
+          string concatText = arrayWords[i].ToLower() + arrayWords[j].ToLower();
+          string letterRest = wordToFind;
+
+          if (concatText.Length != wordToFind.Length)
+            continue;
+
+          foreach (var letter in concatText)
+          {
+            if (letterRest.IndexOf(letter) > -1)
+            {
+              letterRest = letterRest.Remove(letterRest.IndexOf(letter), 1);
+            }
+
+            if (letterRest.Length == 0)
+            {
+              matchWords.Add(new string[2] { arrayWords[i], arrayWords[j] });
+              break;
+            }
+          }
+        }
+      }
+      Console.WriteLine(matchWords);
+    }
+  }
+}
