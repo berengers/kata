ef7d8430eac3f95d693accae9c3eef31ca3a0bf5
Berenger <berenger.salmon@gmail.com>
2019-04-03 16:49:15 +0200

    refacto: move datas to InlineData

--- RomanBills/RomanBills.Test/UnitTest1.cs
+++ RomanBills/RomanBills.Test/UnitTest1.cs
@@ -5,16 +5,16 @@ namespace RomanBills.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test_Convert_Roman_To_Integer__Should_Return_True()
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("XV", 15)]
+        [InlineData("IX", 9)]
+        [InlineData("-IX", -9)]
+        [InlineData("-MDCLXVI", -1666)]
+        [InlineData("-MCDLXVI", -1466)]
+        public void Test_Convert_Roman_To_Integer__Should_Return_True(string romanSigns, int value)
         {
-            Assert.Equal(Program.convertRomanToInteger(""), 0);
-            Assert.Equal(Program.convertRomanToInteger("-"), 0);
-            Assert.Equal(Program.convertRomanToInteger("XV"), 15);
-            Assert.Equal(Program.convertRomanToInteger("VX"), 5);
-            Assert.Equal(Program.convertRomanToInteger("-VX"), -5);
-            Assert.Equal(Program.convertRomanToInteger("-MDCLXVI"), -1666);
-            Assert.Equal(Program.convertRomanToInteger("-MCDLXVI"), -1466);
+            Assert.Equal(Program.convertRomanToInteger(romanSigns), value);
         }
     }
 }
